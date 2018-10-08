a5f9c03786232ffaa947403f1e65e2795ac6742a
DengZy <2657179843@qq.com>
2018-10-08 17:38:14 +0800

    增加绘制箭头函数

--- 自定义封装库及测试dome/Graphical.js
+++ 自定义封装库及测试dome/Graphical.js
@@ -35,3 +35,59 @@ Ball.prototype = {
 		cxt.restore();
 	}
 }
+
+/*
+* 功能：绘制箭头
+* 传入参数： x:箭头中心x轴坐标
+			y：箭头中心y轴坐标
+			color:颜色
+			angle:旋转角度
+* 时间：2018-10-8
+*/
+function Arrow(x, y, color, angle) {
+    //箭头中心x坐标，默认值为0
+    this.x = x || 0;
+    //箭头中心y坐标，默认值为0
+    this.y = y || 0;
+    //颜色，默认值为“#FF0099”
+    this.color = color || "#FF0099";
+    //旋转角度，默认值为0
+    this.angle = angle || 0;
+}
+Arrow.prototype = {
+    stroke: function (cxt) {
+        cxt.save();
+        cxt.translate(this.x, this.y);
+        cxt.rotate(this.angle);
+        cxt.strokeStyle = this.color;
+        cxt.beginPath();
+        cxt.moveTo(-20, -10);
+        cxt.lineTo(0, -10);
+        cxt.lineTo(0, -20);
+        cxt.lineTo(20, 0);
+        cxt.lineTo(0, 20);
+        cxt.lineTo(0, 10);
+        cxt.lineTo(-20, 10);
+        cxt.closePath();
+        cxt.stroke();
+        cxt.restore();
+    },
+    fill: function (cxt) {
+        cxt.save();
+        cxt.translate(this.x, this.y);
+        cxt.rotate(this.angle);
+        cxt.fillStyle = this.color;
+        cxt.beginPath();
+        cxt.moveTo(-20, -10);
+        cxt.lineTo(0, -10);
+        cxt.lineTo(0, -20);
+        cxt.lineTo(20, 0);
+        cxt.lineTo(0, 20);
+        cxt.lineTo(0, 10);
+        cxt.lineTo(-20, 10);
+        cxt.closePath();
+        cxt.fill();
+        cxt.restore();
+    }
+};
+
